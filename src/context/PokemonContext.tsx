c62edd175c0f6679630552fc955a982a6efd3dfa
Murage-Ivy <ivymurage2000@gmail.com>
2023-04-06 15:26:51 +0300

    Add a call back function

--- src/context/PokemonContext.tsx
+++ src/context/PokemonContext.tsx
@@ -69,6 +69,7 @@ export const PokemonContextProvider = ({ children }: PokeContextProviderProps) =
         fetchPokemons()
 
     }, [])
+    
     useEffect(() => {
         pokemons.map(pokemon => fetchPokemon(pokemon.url))
     }, [pokemons, fetchPokemon])
